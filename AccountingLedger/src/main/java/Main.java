dbab824a47f859fa55d2cc98e52fc283accb4e86
krosengr4 <rosenkev4@gmail.com>
2025-04-25 15:59:33 -0500

    Added empty method to print reports screen. Added requirements comments for ledgerScreen() and reportsScreen()

--- AccountingLedger/src/main/java/Main.java
+++ AccountingLedger/src/main/java/Main.java
@@ -63,7 +63,26 @@ R) Reports - A new screen that allows the user to run pre-defined reports or to 
     }
 
     private static void ledgerScreen() {
+        /*
+        A) All - Display all entries
+        D) Deposits - Display only the entries that are deposits into the account
+        P) Payments - Display only the negative entries (or payments)
+        R) Reports - A new screen that allows the user to run pre-defined reports or to run a custom search:
+         */
         System.out.println("Ledger screen");
+    }
+
+    private static void reportsScreen() {
+        /*
+        1) Month To Date
+        2) Previous Month
+        3) Year To Date
+        4) Previous Year
+        5) Search by Vendor - prompt the user for the vendor name and display all entries for that vendor
+        0) Back - go back to the report page
+        H) Home - go back to the home page
+         */
+        System.out.println("Reports Screen");
     }
 
 }
