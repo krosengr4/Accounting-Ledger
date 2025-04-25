31df9fcc13037282f444be88abc26703abc42250
krosengr4 <rosenkev4@gmail.com>
2025-04-25 14:58:32 -0500

    Added utils class with method to prompt user and get user input

--- AccountingLedger/src/main/java/Utils.java
+++ AccountingLedger/src/main/java/Utils.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class Utils {
 
-    //Prompt user for input and return user input
-    static String getUserInput(String message) {
+    //Prompt user and return user input
+    static String promptAndGetUserInput(String message) {
         Scanner myScanner = new Scanner(System.in);
         System.out.println(message);
         String userInput = myScanner.nextLine();
