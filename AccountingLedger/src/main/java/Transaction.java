6800dd76c7c10058689ea2f749b83bdf5da54805
krosengr4 <rosenkev4@gmail.com>
2025-04-26 07:38:25 -0500

    Created constructor + getters and setters for Transaction in Transaction.java class

--- AccountingLedger/src/main/java/Transaction.java
+++ AccountingLedger/src/main/java/Transaction.java
@@ -1,2 +1,56 @@
 public class Transaction {
+    //declare variables for constructor
+    String date;
+    String time;
+    String description;
+    String vendor;
+    double amount;
+
+    //Constructor to create new Transaction (instances of)
+    public Transaction(String date, String time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    //region Getters and Setters
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+    //endregion
+
+
 }
